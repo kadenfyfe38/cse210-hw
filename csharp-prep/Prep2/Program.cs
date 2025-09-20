30ba8df11a0dde1cf1a3123e3e9b07496f468d35
Kaden <fyf24001@byui.edu>
2025-09-19 19:45:28 -0600

    Completed the Prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string ValueFromUser = Console.ReadLine();
+
+        int NumGrade = int.Parse(ValueFromUser);
+        string letter = "";
+
+
+        if (NumGrade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (NumGrade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (NumGrade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (NumGrade >= 60)
+        {
+            letter = "D";
+        }
+
+        else //(NumGrade < 60)
+        {
+            letter = "F";
+        }
+
+        if (NumGrade >= 70)
+        {
+            Console.WriteLine($"Congrats! You passed with a grade of {NumGrade}!");
+        }
+
+        else
+        {
+            Console.WriteLine("You'll get 'em next time.");
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
     }
 }
