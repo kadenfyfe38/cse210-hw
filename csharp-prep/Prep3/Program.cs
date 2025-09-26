ec5ef3b6dd47641ef0860490d305caa92772ed67
Kaden <fyf24001@byui.edu>
2025-09-25 19:01:16 -0600

    completed core requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic num ");
+        //string magicNum = Console.ReadLine();
+        //int magicNumInt = int.Parse(magicNum);
+
+        Random randomGenerator = new Random();
+        int magicNumInt = randomGenerator.Next(1, 101);
+
+        string response;
+
+        
+        do //loop to check if response == magic num or not
+        {
+            Console.Write("What is your guess? ");
+            response = Console.ReadLine();
+            int responseInt = int.Parse(response);
+
+            if (responseInt < magicNumInt) //checks if response is less than num
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (responseInt > magicNumInt) //checks if response is greater than num
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!"); //prints message if they got it right
+            }
+
+        } while (int.Parse(response) != magicNumInt); //ends loop if they guessed it
+
+
     }
 }
