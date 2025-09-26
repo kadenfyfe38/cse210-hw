4f8e0b3ce0480cf09035531b7f887de4541d030c
Kaden <fyf24001@byui.edu>
2025-09-25 20:29:58 -0600

    completed core requirements

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int sum = 0; //init sum
+        float average = 0; //init avg
+        int runningTotal = 0; //init running total for list
+        int response = 1; //init response
+        int maxNum = 0; //init maximum number
+
+        do
+        {
+            Console.Write("Please add a number: ");
+            string responseStr = Console.ReadLine();
+            response = int.Parse(responseStr);
+            numbers.Add(response);
+            runningTotal ++; //adds 1 to running total
+
+
+        } while (response != 0);
+        runningTotal = runningTotal - 1; //subtracts 1 from running total to account for user entering 0
+        foreach (int num in numbers)
+        {
+            sum = sum += num; //adds sum to total sum
+            if (num > maxNum)
+            {
+                maxNum = num;
+            }
+        }
+        average = (float)sum / (float)runningTotal; //compute average 
+
+        Console.WriteLine($"Sum: {sum}\nAverage: {average}\nLargest Number: {maxNum}"); 
     }
 }
